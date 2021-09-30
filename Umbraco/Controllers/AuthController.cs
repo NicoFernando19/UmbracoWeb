63e6d6f8ed47b8baee799c9bcbcfa2418a1b0824
NicoFernando19 <nicofernando39@gmail.com>
2021-09-30 15:45:16 +0700

    learned about CRUD functionality using NPoco

--- Umbraco/Controllers/AuthController.cs
+++ Umbraco/Controllers/AuthController.cs
@@ -2,6 +2,9 @@
 using System.Web.Mvc;
 using Umbraco.Web.Models;
 using Umbraco.Models;
+using Umbraco.Databases.Entities;
+using NPoco;
+using System.Collections.Generic;
 
 namespace Umbraco.Controllers
 {
@@ -18,6 +21,18 @@ namespace Umbraco.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult LoginPost(LoginFormModel loginFormModel)
         {
+            var employee = new Department() {
+                Name = "IT 2",
+                Description= "This is It Department 2"
+            };
+
+            IDatabase db = new Database("umbracoDbDSN");
+            //Department employee = db.SingleById<Department>(1); // get by id
+            //db.Update(employee); //update
+            //db.Delete(employee); //delete
+            //db.Insert(employee); // insert
+            //List<Department> users = db.Fetch<Department>("select * from Department"); //get
+
             return RedirectToCurrentUmbracoPage();
         }
     }
